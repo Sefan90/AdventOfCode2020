8938a8ae12ef89ea10b4960f8725ba01f0d6a17c
Stefan <stevetv90@gmail.com>
2020-12-01 06:36:02 +0100

    Day1 done

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,3 +1,17 @@
 def Part1():
-    print("Hi")
-Part1()
+    lines = open("input.txt","r").readlines() 
+    for i in lines:
+        for j in lines:
+            if int(i)+int(j) == 2020:
+                print(int(i)*int(j))
+                quit()
+def Part2():
+    lines = open("input.txt","r").readlines()
+    for i in lines:
+        for j in lines:
+            if int(i)+int(j) < 2020:
+                for k in lines:
+                    if int(i)+int(j)+int(k) == 2020:
+                        print(int(i)*int(j)*int(k))
+                        quit()
+Part2()
