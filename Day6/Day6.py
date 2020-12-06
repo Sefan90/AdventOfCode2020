5b56f754d4fb48aefd4269c2a0e221e93922d355
Stefan <stevetv90@gmail.com>
2020-12-06 11:34:35 +0100

    Update Day6.py

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -5,14 +5,14 @@ def Part1():
     sum = 0
     for i in lines:
         if i in ['\n', '\r\n']:
-            sum += yes.count(True);
+            sum += yes.count(True)
             yes = [False]*26
             continue
         else:
             for l in i:
                 if l != '\n':
                     yes[ord(l)-97] = True
-    sum += yes.count(True);
+    sum += yes.count(True)
     print(sum)
 
 def Part2():
@@ -22,14 +22,14 @@ def Part2():
     sum = 0
     for i in lines:
         if i in ['\n', '\r\n']:
-            sum += yes.count(True);
+            sum += yes.count(True)
             yes = [True]*26
             continue
         else:
             for l in range(len(yes)):
                 if chr(l+97) not in i:
                     yes[l] = False
-    sum += yes.count(True);
+    sum += yes.count(True)
     print(sum)
 
 import time
