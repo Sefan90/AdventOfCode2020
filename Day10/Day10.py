5a839245f7963815cd59849717c83bd50524345e
Stefan <stevetv90@gmail.com>
2020-12-10 19:18:50 +0100

    Day10 Done

--- Day10/Day10.py
+++ Day10/Day10.py
@@ -1,5 +1,3 @@
-import itertools
-import math
 def Part1():
     f = open("input.txt","r")
     currentsum = 0
@@ -10,29 +8,24 @@ def Part1():
         currentsum = i
     print(jolt[0]*jolt[2])
 
-def Connect(inputlist):
-    currentsum = 0
-    jolt = [0,0,1] #your device's built-in adapter is always 3 higher than the highest adapter
-    for i in inputlist:
-        if i - currentsum-1 in [0,1,2]:
-            jolt[i - currentsum-1] += 1
-            currentsum = i
-        else:
-            return -1
-    return currentsum
-
 def Part2():
     f = open("input.txt","r")
-    result =[]
-    inputlist = sorted([int(i) for i in f.readlines()])
-    maxlist = max(inputlist)
-    for i in range(math.floor(maxlist/3),len(inputlist)+1):
-        for subset in itertools.combinations(inputlist, i):
-            print(subset)
-            if max((abs(x - y) for (x, y) in zip(subset[1:], subset[:-1])), default=4) < 4 and min(subset, default = 4) < 4 and max(subset, default = 0) == maxlist:
-                result.append(19)
-    print(result.count(maxlist)) 
-
+    inputlist = [0] + sorted([int(i) for i in f.readlines()])
+    inputlist.append(max(inputlist)+3)
+    result = 1
+    counter = 0
+    for i in range(len(inputlist)-1):
+        if inputlist[i+1] - inputlist[i] == 1:
+            counter += 1
+        elif inputlist[i+1] - inputlist[i] == 3:
+            if counter == 2:
+                result *= 2
+            elif counter == 3:
+                result *= 4
+            elif counter == 4:
+                result *= 7
+            counter = 0
+    print(result)
 
 import time
 start_time = time.time()
