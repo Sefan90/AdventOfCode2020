79be3db7253fd9508e15dc7345ebb2bd3722e07a
Stefan <stevetv90@gmail.com>
2020-12-15 18:06:12 +0100

    Part2 done

--- Day15/Day15.py
+++ Day15/Day15.py
@@ -20,4 +20,28 @@ def Part1():
             lista.append(posdiff)
     print(lista[-1])
 
-Part1()
+def Part2():
+    f = open("input.txt","r")
+    inputlist = f.readlines()
+    lista = [int(c) for c in [i.split(",") for i in inputlist][0]]
+    count = {}
+    pos1 = {}
+    pos2 = {}
+    lastnr = lista[-1]
+    for i in range(len(lista)):
+        count[lista[i]] = 1
+        pos1[lista[i]] = i
+    for i in range(len(lista),30000000):
+        if count[lastnr] == 1:
+            lastnr = 0
+        else:
+            lastnr = pos1[lastnr] - pos2[lastnr]
+        if lastnr in count.keys():
+            count[lastnr] += 1
+        else:
+            count[lastnr] = 1
+        pos2[lastnr] = pos1.get(lastnr)
+        pos1[lastnr] = i
+    print(lastnr)
+
+Part2()
