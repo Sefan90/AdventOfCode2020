574f3f7d4d42c6a5dc426c7246484ae724aa5945
Stefan <stevetv90@gmail.com>
2020-12-16 20:22:59 +0100

    Day 16 done

--- Day16/Day16.py
+++ Day16/Day16.py
@@ -21,7 +21,7 @@ def Part1():
     print(result)
 
 def Part2():
-    f = open("testinput.txt","r")
+    f = open("input.txt","r")
     inputlist = f.readlines()
     prev = ""
     rule = {}
@@ -32,25 +32,64 @@ def Part2():
             prev = row
             continue
         elif prev == "your ticket:\n":
-            myticket = [i.replace("\n","") for i in row.split(",")]
+            myticket = [int(i) for i in row.split(",")]
             continue
         elif ":" in row:
-            rule[row.split(":")[0]] = [row.split(":")[1].split()[0].split("-")[0],row.split(":")[1].split()[0].split("-")[1],row.split(":")[1].split()[-1].split("-")[0],row.split(":")[1].split()[-1].split("-")[1]]
+            templist = []
+            for i in range(int(row.split(":")[1].split()[0].split("-")[0]),int(row.split(":")[1].split()[0].split("-")[1])+1):
+                templist.append(i)
+            for i in range(int(row.split(":")[1].split()[-1].split("-")[0]),int(row.split(":")[1].split()[-1].split("-")[1])+1):
+                templist.append(i)
+            rule[row.split(":")[0]] = templist
         else:
             nearbytickets[row] = [int(i) for i in row.split(",")]
 
+    # Removes nearbytickets which aren't valid for any field
     delete = []
     for k,v in nearbytickets.items():
         for i in v:
-            if i not in rule:
+            found = False
+            for _,rv in rule.items():
+                if i in rv:
+                    found = True
+            if found == False:
                 delete += [k]
-                break
     for d in delete:
-        del nearbytickets[d]
+        if d in nearbytickets.keys():
+            del nearbytickets[d]
 
-    result = 0
-    print(myticket)
-    print(nearbytickets)
-    print(rule)
+    # Creates a dict with all Rules and possibel placements for them
+    ruledict = {}
+    for rk,rv in rule.items():
+        rulelist = []
+        for i in range(len(list(nearbytickets.values())[0])):
+            found = True
+            for key in nearbytickets.keys():
+                if nearbytickets.get(key)[i] not in rv:
+                    found = False
+            if found == True:
+                rulelist.append(i)
+        ruledict[rk] = rulelist
+    
+    # Puts the Rules in the correct order
+    resultlist = []
+    for _ in ruledict.items():
+        resultlist.append("")
+    while "" in resultlist:
+        for k,v in ruledict.items():
+            if len(v) == 1:
+                resultlist[v[0]] = k
+                for rk,rv in ruledict.items():
+                    if v[0] in rv and k != rk:
+                        rv.remove(v[0])
+                        ruledict[rk] = rv
+                v = []
+
+    result = 1
+    for i, r in enumerate(resultlist):
+        if "departure" in r:
+            result *= myticket[i]
+
+    print(result)
 
 Part2()
