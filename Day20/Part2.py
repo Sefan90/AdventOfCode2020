61b372b3f545137906e94437fb5b1c7265349797
Stefan <stevetv90@gmail.com>
2020-12-21 19:29:51 +0100

    Update Part2.py

--- Day20/Part2.py
+++ Day20/Part2.py
@@ -15,6 +15,7 @@ for row in file:
         tmpdict["B"] = imagetile[-1]
         tmpdict["R"] = "".join([i[-1:]for i in imagetile])
         tmpdict["L"] = "".join([i[:1]for i in imagetile])
+        tmpdict["I"] = imagetile   
         tiledict[currentkey] = tmpdict
     else:
         imagetile += [row.replace("\n","")]
@@ -26,20 +27,37 @@ for key, value in tiledict.items():
                 break
             elif key != ikey:
                 for ik, iv in ivalue.items():
-                    if v == iv or v == iv[len(iv)::-1]:
-                        matchdict[key].append([k,ikey,ik])
+                    if v == iv:
+                        matchdict[key].append([k,ikey,ik,False])
+                        matchfound = True
+                        break
+                    elif v == iv[len(iv)::-1]:
+                        matchdict[key].append([k,ikey,ik,True])
                         #matchdict[ikey].append([ik,key,k])
                         matchfound = True
                         break
 #print(tiledict)
 print(matchdict)
 
-corner = ""
+currentTile = ""
 for k,v in matchdict.items():
     if len(v) == 2:
         summa *= int(k)
-        corner = k
+        if currentTile == "" and ((v[0][0] == "B" and v[1][0] == "R" and v[0][3] == False) or (v[0][0] == "R" and v[1][0] == "B" and v[0][3] == False)):
+            currentTile = k
 print(summa)
 
-print(matchdict[corner])
+print(matchdict[currentTile])
 
+image = tiledict[currentTile]["I"]
+tilelist = []
+while currentTile not in tilelist:
+    tilelist.append(currentTile)
+    lastitem = []
+    for item in matchdict[currentTile]:
+        if item[0] == "R":
+            lastitem = item
+
+
+
+print(image)
